4742902e06b28d4d45a16b554cdc9b4a11c56785
Jörn <jhdaug@googlemail.com>
2020-11-20 12:28:42 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
@@ -6,6 +7,8 @@ import { InputComponent } from "./components/input-field/input-field.component";
 import { InputButtonComponent } from "./components/button/button.component";
 import { StepComponent } from "./components/step/step.component";
 import { ProgressbarComponent } from "./components/progressbar/progressbar.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { TableComponent } from "./components/table/table.component";
 
 @NgModule({
   declarations: [
@@ -14,8 +17,10 @@ import { ProgressbarComponent } from "./components/progressbar/progressbar.compo
     InputButtonComponent,
     StepComponent,
     ProgressbarComponent,
+    MenuComponent,
+    TableComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
