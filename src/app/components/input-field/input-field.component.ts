6610568c86d86f3d08b81aa83f3ea1e399997a51
Jörn <jhdaug@googlemail.com>
2020-11-20 12:30:47 +0100

    added dropdown and cleaned up

--- src/app/components/input-field/input-field.component.ts
+++ src/app/components/input-field/input-field.component.ts
@@ -6,33 +6,103 @@ import {
   ViewChild,
   AfterViewInit,
   ChangeDetectionStrategy,
+  SimpleChanges,
+  OnChanges,
 } from "@angular/core";
-import { Step } from 'src/app/service/step.service';
+import { Step } from "src/app/service/step.service";
 
 @Component({
   selector: "rating-input",
   templateUrl: "./input-field.component.html",
   styleUrls: ["./input-field.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent implements AfterViewInit {
-
+export class InputComponent implements OnChanges, AfterViewInit {
   @Input() step: Step;
+  @Input() result: number;
+  @Input() helperResult: number;
+  @Output() valueChanged: EventEmitter<{
+    [key: string]: number;
+  }> = new EventEmitter<{ [key: string]: number }>(undefined);
 
-  @Input() value: number | string;
-  @Input() helperValue: number | string;
-  @Output() valueChanged: EventEmitter<{[key: string]: number}> = new EventEmitter<{[key: string]: number}>(undefined);
+  public showDropdown = false;
+  public dropdownClicked = false;
 
   @ViewChild("inputField", { static: false }) inputRef;
 
-  ngAfterViewInit() {
-    this.inputRef.nativeElement.focus(); // als Directive? ja, da gegeg SRP
-
+  ngOnChanges(change: SimpleChanges) {
+    this.focusInput(change);
+    this.resetInput(change);
   }
 
-  onChange(key, value) {
-    if (!!this.inputRef.nativeElement && (!!value || value === 0)) {
-      this.valueChanged.emit({key, value: Number(value)});
+  ngAfterViewInit() {
+    this.focusInput();
+  }
+
+  private focusInput(change?: SimpleChanges): void {
+    if (this.changedStep(change) || this.hasEmptyHelperResult(change)) {
+      if (!!this.inputRef && !!this.inputRef.nativeElement) {
+        this.inputRef.nativeElement.focus();
+        return;
+      }
     }
+
+    if (!change) {
+      this.inputRef.nativeElement.focus();
+    }
+  }
+
+  private changedStep(change: SimpleChanges): boolean {
+    return (
+      !!change &&
+      !!change.step &&
+      !!change.step.previousValue &&
+      change.step.previousValue.id !== change.step.currentValue.id
+    );
+  }
+
+  private hasEmptyHelperResult(change: SimpleChanges): boolean {
+    return (
+      !!change && !!change.helperResult && change.helperResult.currentValue === undefined
+    );
+  }
+
+  private resetInput(change?: SimpleChanges): void {
+    if (!!change.step && change.step.currentValue.id === 2) {
+      this.result = undefined;
+      this.helperResult = undefined;
+    }
+  }
+
+  public onChange(key, value: number): void {
+    if (key === "gameResult") {
+      value = this.validateGameResult(value);
+    }
+
+    if (!!value || value === 0) {
+      this.emitValue(key, value);
+    }
+  }
+
+  private validateGameResult(value: number): number {
+    if (!!value || value === 0) {
+      value = Number(("" + value).replace(/,/g, "."));
+
+      const regex = new RegExp("^[01]$|^0.5*$", "g");
+      if (!regex.test("" + value)) {
+        value = 1;
+      }
+    }
+
+    return value;
+  }
+
+  private emitValue(key, value: number): void {
+    this.dropdownClicked = true;
+    this.valueChanged.emit({ key, value: Number(value) });
+  }
+
+  public showOrHideDropdown(): void {
+    this.showDropdown = !this.showDropdown;
   }
 }
